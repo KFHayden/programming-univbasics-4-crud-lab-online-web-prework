5df92500c6b1c99619faf3fad0fd279c459272d5
Kevin Hayden <kevin.fhayden@gmail.com>
2019-07-19 20:27:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,13 +3,15 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  ["one", "two", "three", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  ["one", "two", "three", "four"]<<("arrays!")
+end
+
 def add_element_to_start_of_array(array, element)
-  
+  "[ ]
 end
 
 def remove_element_from_end_of_array(array)
